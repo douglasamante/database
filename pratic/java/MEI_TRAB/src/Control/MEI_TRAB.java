2b31f7454632322180fd2457eb5ef55c39449000
Douglas Amante <douglassales@gec.inatel.br>
2020-10-20 21:24:45 +0100

    Update MEI_TRAB.java

--- pratic/java/MEI_TRAB/src/Control/MEI_TRAB.java
+++ pratic/java/MEI_TRAB/src/Control/MEI_TRAB.java
@@ -2,12 +2,21 @@
 package Control;
 
 import View.login;
+import Model.*;
+import java.util.ArrayList;
+
 
 public class MEI_TRAB {
 
     
     public static void main(String[] args) {
+      
+        show S_window = new show();
+        auth Auth = new auth();
+        browser Browser = new browser();
+        edit Edit = new edit();
         
-    }
-    
+        S_window.showLogin(Auth);
+        
+    }    
 }
