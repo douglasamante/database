7bbed0c477dc0b3e6efeaaa31614ab157239fdf4
Douglas Amante <douglassales@gec.inatel.br>
2020-10-22 18:44:12 +0100

    Updated 22/10

---
+++ pratic/java_updated_22_10/MEI_TRAB/src/Model/show.java
@@ -0,0 +1,78 @@
+
+package Model;
+
+import View.*;
+
+public class show {
+    
+    static login Login;
+    static application Application;
+    static edit Edit;
+    static log Log;
+    static time_log TimeLog;
+    static browser Browser;
+    
+    public void showLogin(auth Auth){
+        
+        show.Login = new login(Auth);
+        show.Login.setLocationRelativeTo(null);
+        show.Login.setVisible(true);
+        
+    }
+    
+    public void unshowLogin(auth Auth){
+        show.Login.setVisible(false);
+    }
+    
+    public void showApplication(){
+        show.Application = new application();
+        show.Application.setLocationRelativeTo(null);
+        show.Application.setVisible(true);
+    }
+    
+    public void unshowApplication(){
+        show.Application.setVisible(false);
+    }
+    
+    
+    public void showEdit(){
+        show.Edit = new edit();
+        show.Edit.setLocationRelativeTo(null);
+        show.Edit.setVisible(true);
+    }
+    
+    public void unshowEdit(){
+        show.Edit.setVisible(false);
+    }
+    
+    public void showBrowser(){
+        show.Browser = new browser();
+        show.Browser.setLocationRelativeTo(null);
+        show.Browser.setVisible(true);
+    }
+    
+    public void unshowBrowser(){
+        show.Browser.setVisible(false);
+    }
+    
+    public void showLog(){
+        show.Log = new log();
+        show.Log.setLocationRelativeTo(null);
+        show.Log.setVisible(true);
+    }
+    
+    public void unshowLog(){
+        show.Log.setVisible(false);
+    }
+    
+    public void TimeLog(){
+        show.TimeLog = new time_log();
+        show.TimeLog.setLocationRelativeTo(null);
+        show.TimeLog.setVisible(true);
+    }
+    
+    public void unTimeLog(){
+        show.TimeLog.setVisible(false);
+    }
+    
+}
---
+++ pratic/java_updated_22_10/MEI_TRAB/src/Model/users.java
@@ -0,0 +1,49 @@
+
+package Model;
+
+public class users {
+    
+    protected String username; 
+    protected char[] password;
+    protected String host_name;
+    protected String database_name;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+
+    public String getHost_name() {
+        return host_name;
+    }
+
+    public void setHost_name(String host_name) {
+        this.host_name = host_name;
+    }
+
+    public String getDatabase_name() {
+        return database_name;
+    }
+
+    public void setDatabase_name(String database_name) {
+        this.database_name = database_name;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
